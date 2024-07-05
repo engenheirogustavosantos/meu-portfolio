c5ef9bfbd683f3d0572cde1a39ab7ed2d6a9a33a
Gustavo dos Santos C. <gustavo.cssantos2002@gmail.com>
2024-07-05 11:41:59 -0300

    Melhoreu a  implementação do scroll reveal

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,52 @@ window.addEventListener('scroll', function() {
 
 // Scroll reveal
 window.sr = ScrollReveal({reset: true});
-sr.reveal('.max-width', {duration: 1000});
+sr.reveal('.header__right', {duration: 1000, distance: '90px'});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.header__left', {duration: 1000, distance: '90px', delay: 700});
+
+window.sr = ScrollReveal({reset: false});
+sr.reveal('.navbar__content', {duration: 1000, distance: '90px', delay: 300});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.about_left', {duration: 1000, distance: '90px', delay: 300, origin:'left'});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.about_right', {duration: 1000, distance: '90px', delay: 600, origin:'right'});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.about_right', {duration: 1000, distance: '90px', delay: 600, origin:'right'});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.tertiary-title mb-m', {duration: 1000, distance: '90px', delay: 600});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.description', {duration: 1000,distance: '90px', delay: 600});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.skills_item1', {duration: 1000,distance: '90px', delay: 600});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.skills_item2', {duration: 1000,distance: '90px', delay: 800});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.skills_item3', {duration: 1000,distance: '90px', delay: 1000});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.skills', {duration: 1000,distance: '90px', delay: 300});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.project__content', {duration: 1000, distance: '90px', delay: 600});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.projeto1', {duration: 1000,distance: '90px', delay: 1000, origin: 'left'});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.projeto2', {duration: 1000,distance: '90px', delay: 1000, origin: 'right'});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.projeto3', {duration: 1000,distance: '90px', delay: 1000, origin: 'left'});
 
 
 
